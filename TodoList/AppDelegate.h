aa83302c29f5c7577b374d8733a46bbbd9339459
HusterYP <3209605462@qq.com>
2019-01-08 13:55:53 +0800

    Initial Commit

---
+++ TodoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TodoList
+//
+//  Created by yuanping on 2019/1/8.
+//  Copyright © 2019年 yuanping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
